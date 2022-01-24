bfeeac01bb0c218b094ef0ff9103e5c317b7fcd7
Filipe Lourenço <filipe.martins.lourenco@blip.pt>
2022-01-24 12:31:35 +0000

    add transclusion

--- src/index.js
+++ src/index.js
@@ -5,11 +5,19 @@ const isEqual = require("lodash/isEqual");
 
 function angularize(Component, componentName, angularApp, bindings) {
 	bindings = bindings || {};
+	
+	let renderInfo;
+    let transclusion;
+    let element;
+
 	if (typeof window === "undefined" || typeof angularApp === "undefined") return;
 
-	angularApp.component(componentName, {
+	angularApp
+	.component(componentName, {
 		bindings,
 		controller: ["$element", function ($element) {
+            element = $element;
+
 			if (window.angular) {
 				// Add $scope
 				this.$scope = window.angular.element($element).scope();
@@ -41,10 +49,109 @@ function angularize(Component, componentName, angularApp, bindings) {
 			}
 
 			this.$onChanges = () => {
-				ReactDOM.render(React.createElement(Component, this), $element[0]);
+				renderInfo = _this;
+                render();
 			};
 		}]
 	})
+	.directive(componentName, function () {
+		return {
+			restrict: 'EA',
+			transclude: true,
+
+			link: function (scope, element, attributes, _, transclude) {
+				const children = transclude();
+				transclusion = createReactElements(children);
+				render();
+
+				// as seen in react2angularjs
+				function createReactElements(elements) {
+					let reactElements = Array.from(elements).reduce(createElement, []);
+
+					//If there is only one Child, react expect children object instead of Array
+					if (reactElements.length === 1) {
+						return reactElements[0];
+					}
+
+					if (reactElements.length === 0) {
+						return undefined;
+					}
+
+					return reactElements;
+				}
+				// as seen in react2angularjs
+				function findReactComponent(el) {
+					let key = Object.keys(el).find(key =>
+						key.startsWith('__reactInternalInstance$')
+					);
+
+					if (el[key]) {
+						let fiberNode = el[key];
+						return fiberNode && fiberNode.return; //fiberNode.return && fiberNode.return.stateNode;
+					}
+
+					return null;
+				}
+				// as seen in react2angularjs
+				function createElement(elements, element, index) {
+					if (element instanceof HTMLElement) {
+						let existing = findReactComponent(element);
+
+						if (!existing) {
+							let reactElement = createHTMLElement(element, index);
+							elements.push(reactElement);
+						}
+
+					} else if (element.nodeName === '#text') {
+						let reactElement = createTextElement(element);
+						//replace new line characters with empty space
+						let textContent = reactElement.replace(/âµ/, '');
+
+						//Add only if element contains text content
+						if (textContent) {
+							elements.push(reactElement);
+						}
+					}
+
+					return elements;
+				}
+				// as seen in react2angularjs
+				function createHTMLElement(element, index) {
+					let { tagName, childNodes } = element;
+					let tagname = tagName.toLowerCase();
+					let attributes = getAttributesMap(element);
+
+					attributes.key = index;
+					attributes.className = attributes.class;
+					delete attributes.class;
+
+					if (childNodes.length > 0) {
+						attributes.children = createReactElements(childNodes);
+					}
+
+					return React.createElement(tagname, attributes);
+				}
+				// as seen in react2angularjs
+				function createTextElement(element) {
+					return element.data.trim();
+				}
+				// as seen in react2angularjs
+				function getAttributesMap(element) {
+					let { attributes } = element;
+					let attrArray = Array.from(attributes || []);
+
+					return attrArray.reduce((map, attr) => {
+						map[attr.name] = attr.value;
+						return map;
+					}, {});
+				}
+			}
+		}
+	});
+
+	function render() {
+        ReactDOM.render(React.createElement(Component, renderInfo, transclusion), element[0])
+    }
 }
 
 function angularizeDirective(Component, directiveName, angularApp, bindings) {
